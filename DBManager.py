b46f2d9d88f35f71e1b886b3f517b3a510c6579a
PalaciosYoan <ypalaciosguzman@ucmerced.edu>
2021-10-20 09:39:21 -0700

    started db Class using sqlite3. Will uprade to mySQL later on

---
+++ DBManager.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn  = sqlite3.connect('db')
+
+c = conn.cursor()
+
+c.execute('''
+          CREATE TABLE IF NOT EXISTS products
+          ([product_id] INTEGER PRIMARY KEY, [product_name] TEXT)
+          ''')
+          
+c.execute('''
+          CREATE TABLE IF NOT EXISTS prices
+          ([product_id] INTEGER PRIMARY KEY, [price] INTEGER)
+          ''')
+                     
+conn.commit()
