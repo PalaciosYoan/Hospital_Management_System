2456dabd29236833d8bd647ed6fe52f8709602a2
Aayush <you@example.com>
2021-10-26 23:03:44 -0700

    added uuid support

--- Phase_2/HMS.sql
+++ Phase_2/HMS.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Hospital(
-    h_id INT UNSIGNED NOT NULL PRIMARY KEY,
+    h_id CHAR(255) NOT NULL PRIMARY KEY,
     address CHAR(255) NOT NULL,
     name CHAR(255) NOT NULL
 );
 
 CREATE TABLE Doctor(
-    d_id INT UNSIGNED NOT NULL PRIMARY KEY,
+    d_id CHAR(255) NOT NULL PRIMARY KEY,
     name CHAR(255) NOT NULL,
     started_working DATE NOT NULL,
     phone_number CHAR(255) NOT NULL,
@@ -14,21 +14,21 @@ CREATE TABLE Doctor(
 );
 
 CREATE TABLE Maintenance(
-    maint_id INT UNSIGNED NOT NULL PRIMARY KEY,
+    maint_id CHAR(255) NOT NULL PRIMARY KEY,
     name CHAR(255) NOT NULL,
     started_working DATE NOT NULL,
     duty CHAR(255) NOT NULL
 );
 
 CREATE TABLE `Hospital-Maintenance Junction Table`(
-    h_id INT UNSIGNED NOT NULL,
-    maint_id INT UNSIGNED NOT NULL,
+    h_id CHAR(255) NOT NULL,
+    maint_id CHAR(255) NOT NULL,
     FOREIGN KEY(h_id) REFERENCES Hospital(h_id),
     FOREIGN KEY(maint_id) REFERENCES Maintenance(maint_id)
 );
 
 CREATE TABLE Medication(
-    m_id INT NOT NULL PRIMARY KEY,
+    m_id CHAR(255) NOT NULL PRIMARY KEY,
     cost DECIMAL(8, 2) NOT NULL,
     name CHAR(255) NOT NULL,
     type CHAR(255) NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE Medication(
     FOREIGN KEY(h_id) REFERENCES Hospital(h_id)
 );
 CREATE TABLE Nurse(
-    n_id INT UNSIGNED NOT NULL PRIMARY KEY,
+    n_id CHAR(255) NOT NULL PRIMARY KEY,
     started_working DATE NOT NULL,
     name CHAR(255) NOT NULL,
     h_id INT NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE Nurse(
 );
 
 CREATE TABLE room(
-    r_id INT UNSIGNED NOT NULL PRIMARY KEY,
+    r_id CHAR(255) NOT NULL PRIMARY KEY,
     room_number INT NOT NULL,
     person_allowed INT NOT NULL,
     cost DECIMAL(8, 2) NOT NULL,
@@ -55,14 +55,14 @@ CREATE TABLE room(
 );
 
 CREATE TABLE `Nurse-Room Junction Table`(
-    r_id INT UNSIGNED NOT NULL,
-    n_id INT UNSIGNED NOT NULL,
+    r_id CHAR(255) NOT NULL,
+    n_id CHAR(255) NOT NULL,
     FOREIGN KEY(r_id) REFERENCES room(r_id),
     FOREIGN KEY(n_id) REFERENCES Nurse(n_id)
 
 );
 CREATE TABLE Patient(
-    p_id INT UNSIGNED NOT NULL PRIMARY KEY,
+    p_id CHAR(255) NOT NULL PRIMARY KEY,
     dob DATE NOT NULL,
     admit_date DATE NOT NULL,
     released_date DATE NOT NULL,
@@ -77,7 +77,7 @@ CREATE TABLE Patient(
 );
 
 CREATE TABLE `Prescribed Med`(
-    pmed_id INT UNSIGNED NOT NULL PRIMARY KEY,
+    pmed_id CHAR(255) NOT NULL PRIMARY KEY,
     assigned_date DATE NOT NULL,
     p_id INT NOT NULL,
     m_id INT NOT NULL,
--- Phase_2/update.sql
+++ Phase_2/update.sql
@@ -1,4 +1,4 @@
 INSERT INTO Hospital(h_id, address, name)
-VALUES(1, 'Lake Road', 'UC_MERCED');
+VALUES('484914f7-b7fd-452a-b044-36ee9fb5ec94', '1250 E Almond Ave, Madera, CA 93637', 'Madera Community Hospital');
 
 SELECT * FROM Hospital;
