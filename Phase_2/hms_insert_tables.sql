8b47e3d59bf4f650bab1370ad0257073b54da9a6
Aayush <koiralaaayush1@gmail.com>
2021-11-15 22:36:26 -0800

    added few insert & uuid

--- Phase_2/hms_insert_tables.sql
+++ Phase_2/hms_insert_tables.sql
@@ -7,3 +7,6 @@ INSERT INTO Hospital(h_id, address, name) VALUES("8644e1e7-c1cd-4d06-a341-de7ff8
 INSERT INTO Hospital(h_id, address, name) VALUES("5ea136bc-a263-44cf-83f7-8ce473526164", "73 Oak Ave. Eastlake, OH 44095", "SHELBY BAPTIST MEDICAL CENTER");
 INSERT INTO Hospital(h_id, address, name) VALUES("73b2e004-7096-4be4-81e5-e6c0c38e214f", "947 Primrose Road Hobart, IN 46342", "GREENE COUNTY HOSPITAL CAH");
 INSERT INTO Hospital(h_id, address, name) VALUES("2a502f14-7102-4aa1-8544-75ce9120f319", "14 Harvard St. Griffin, GA 30223", "TWIN RIVERS REGIONAL MEDICAL CENTER");
+
+INSERT INTO Doctor(d_id, name, started_working, phone_number, h_id) VALUES("9a41e1d6-6a82-4dea-b399-92472277352c", "Joe Rib", "11/14/2020", "3263096506", "18e0672b-e749-4965-b3f7-8bfda0aa23e5");
+INSERT INTO Doctor(d_id, name, started_working, phone_number, h_id) VALUES("d4a5749f-4521-49a1-91e1-4d57d37c1f6d", "Roger Jae", "11/14/2021", "4324556631", "08ff82be-3ec7-4df0-b1eb-6cd9debbb045");
