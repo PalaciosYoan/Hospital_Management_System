be5c97ebdafd1cedd0ee6d3eada271723ca75770
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2021-11-24 00:13:46 -0800

    connected quries classes to Data_Base_Manager.py

--- Phase_3/Data_Base_Manager.py
+++ Phase_3/Data_Base_Manager.py
@@ -2,13 +2,11 @@ from sqlite3 import Error
 from db_packages.Set_Connection import Set_Connection
 from db_packages.Deleting_Queries import Deleting_Queries
 from db_packages.Inserting_Queries import Inserting_Queries
-
-class Data_Base_Manager(Set_Connection, Deleting_Queries, Inserting_Queries):
+from db_packages.Query_Queries import Query_Queries
+from db_packages.Update_Queries import Update_Queries
+class Data_Base_Manager(Set_Connection, Deleting_Queries, Inserting_Queries, Query_Queries, Update_Queries):
     def __init__(self, db_path = r'hsm.db') -> None:
         super(Data_Base_Manager, self).__init__()
-        
-        self.delete_specific_medication('two', 'three')
-        self.insert_hospital('m','2')
         
     def __create_tables(self):
         try:
---
+++ Phase_3/db_packages/Query_Queries.py
@@ -0,0 +1,5 @@
+import sqlite3
+from sqlite3 import Error
+
+class Query_Queries(object):
+    pass
