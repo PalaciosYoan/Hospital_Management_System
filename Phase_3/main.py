73c47ea6327be7e430afea1a55b249ec7bd73d4c
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2021-11-27 14:55:15 -0800

    Added few apis to get different data from tables

--- Phase_3/main.py
+++ Phase_3/main.py
@@ -9,13 +9,49 @@ app = Flask(__name__)
 api = Api(app)
 db_manager = Data_Base_Manager()
 
+class getAllMaintence(Resource):
+    def get(self):
+        df = db_manager.get_maintence()
+        df = json.loads(df.to_json())
+        return df
 class getHospitalData(Resource):
     def get(self):
         df = db_manager.get_hospitals()
         json_data = json.loads(df.to_json())
         return json_data
 
+class getAvaliableMaintence(Resource):
+    def get(self):
+        #must send a json format {'hospital_name':hospital_name}
+        hospital_name = json.loads(request.data)['hospital_name']
+        df = db_manager.get_avaliable_maintence(hospital_name)
+        json_data = json.loads(df.to_json())
+        return json_data
+
+class getDoctors(Resource):
+    def get(self):
+        action = json.loads(request.data)['queryType']
+        if action == 'hospital':
+            hospital_name = json.loads(request.data)['hospital_name']
+            df = db_manager.get_avaliable_maintence(hospital_name)
+            json_data = json.loads(df.to_json())
+            return json_data
+        #elif action =='' # easily be able to add more queries to doctor
+
+class getNurse(Resource):
+    def get(self):
+        action = json.loads(request.data)['queryType']
+        if action == 'hospital':
+            hospital_name = json.loads(request.data)['hospital_name']
+            df = db_manager.get_avaliable_maintence(hospital_name)
+            json_data = json.loads(df.to_json())
+            return json_data
+
 api.add_resource(getHospitalData, '/gethospital')
+api.add_resource(getAllMaintence, '/getAllMaintence')
+api.add_resource(getAvaliableMaintence, '/AvaliableMaintence')
+api.add_resource(getDoctors, '/getDoctors')
+api.add_resource(getNurse, '/getNurse')
 
 @app.route('/')
 def home():
