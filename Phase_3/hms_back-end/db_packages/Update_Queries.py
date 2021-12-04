0782ade79612dd0e63a12040c03553954e441ec3
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2021-12-03 19:38:03 -0800

    update patient's released date and doc

--- Phase_3/hms_back-end/db_packages/Update_Queries.py
+++ Phase_3/hms_back-end/db_packages/Update_Queries.py
@@ -1,3 +1,58 @@
 
+import sqlite3
+from datetime import datetime
+from sqlite3 import Error
+import pandas as pd
+from sympy import E
 class Update_Queries(object):
-    pass
+    ########Patient updates################
+    #release date
+    def update_released_date(self, released_date, name, dob):
+        try:
+            q = """
+                Update
+                    Patient
+                SET 
+                    released_date = "{}"
+                WHERE
+                    dob = "{}" and
+                    name = "{}"
+            """.format(released_date, dob, name)
+            self.conn.execute(q)
+            self.conn.commit()
+        except Error as e:
+            self.conn.rollback()
+            print(e)
+    
+    #doctor
+    def update_doctor(self, d_name_new, name, dob):
+        
+        try:
+            d_name_new = """
+                select d_id
+                FROM Doctor
+                WHERE name = "{}"
+                limit 1;
+            """.format(d_name_new)
+            
+            self.cursor.execute(d_name_new)
+            d_name_new = self.cursor.fetchall()[0][0] #[[d_id]]
+            if d_name_new == '':
+                print("No updates were made because doctor does not exist")
+                return
+
+            q = """
+                Update
+                    Patient
+                SET 
+                    d_id = "{}"
+                WHERE
+                    dob = "{}" and
+                    name = "{}"
+            """.format(d_name_new, dob, name)
+            self.conn.execute(q)
+            self.conn.commit()
+        except Error as e:
+            self.conn.rollback()
+            print(e)
+    
