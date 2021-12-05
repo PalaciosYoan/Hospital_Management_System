ee3d08db3ace15ad3746c1545ab08237a47daec9
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2021-12-04 21:56:02 -0800

    added 1 new data sample to Nurse room junction

--- Phase_3/hms_back-end/a.sql
+++ Phase_3/hms_back-end/a.sql
@@ -52,3 +52,27 @@ from Patient p,
 ) m1
 where
     p.p_id = m1.p_id;
+
+
+select *
+from Patient, 
+    (select p_id 
+    from Room,
+        (
+            select h_id
+            from Hospital
+            where name = "CALLAHAN EYE HOSPITAL"
+        ) h1
+    where Room.h_id = h1.h_id and room.room_number = 2
+    ) t2
+where Patient.p_id = t2.p_id;
+
+select Room.p_id
+from Room, Nurse_Room_Junction_Table t1
+where 
+    Room.p_id;
+
+select Room.r_id
+from Room, Nurse_Room_Junction_Table t1
+where 
+    Room.p_id and t1.r_id = Room.r_id;
