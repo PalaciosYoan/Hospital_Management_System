4da008a7ae8ac71fd0db0e4ce3537afb083e184f
Aayush <koiralaaayush1@gmail.com>
2021-12-04 17:55:39 -0800

    added room menu

---
+++ Phase_3/hms_front-end/src/components/get_hInfo/room_menu.js
@@ -0,0 +1,140 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Grid } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 200,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const cardStyles = makeStyles({
+  gridContainer: {
+    paddingLeft: "20px",
+    paddingRight: "20px",
+  },
+});
+
+function OutlinedCard() {
+  const navigate = useNavigate();
+  const [room_menu, setRoom_Menu] = useState([]);
+
+  useEffect(() => getRoom_Menu(), []);
+  const getRoom_Menu = () => {
+    axios
+      .post("http://127.0.0.1:5000/getRooms", {
+        queryType: "nurse",
+        nurse_name: localStorage.getItem("nurse_name"),
+      })
+      .then(function (response) {
+        console.log(response.data);
+        setRoom_Menu(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  const classes = useStyles();
+  const cards = cardStyles();
+
+  function mapCards(room_menu, index) {
+    return (
+      <Grid item xs={12} sm={6} md={4} key={index}>
+        <Card className={classes.root} variant="outlined">
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            ></Typography>
+            <Typography variant="h5" component="h2">
+              <center>{room_menu.room_number}</center>
+            </Typography>
+            <Typography variant="body2">
+              <center>{room_menu.type}</center>
+            </Typography>
+            <Typography variant="body2">
+              <b>Person Allowed</b> : {room_menu.person_allowed}
+            </Typography>
+            <Typography variant="body2">
+              <b>Cost</b> : ${room_menu.cost}
+            </Typography>
+          </CardContent>
+          <CardActions style={{ justifyContent: "center" }}>
+            <Button
+              onClick={() => {
+                console.log(room_menu.name);
+                navigate("/patient");
+              }}
+              size="small"
+            >
+              show patient
+            </Button>
+            <Button
+              onClick={() => {
+                console.log(room_menu.name);
+                navigate("/patient");
+              }}
+              size="small"
+            >
+              show other nurses
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    );
+  }
+  return (
+    <div>
+      <div>
+        <center>
+          <Card className={cards.root} variant="outlined">
+            <CardContent>
+              <Typography
+                className="Hospital"
+                color="textSecondary"
+                gutterBottom
+              ></Typography>
+              <Typography variant="h5" component="h2">
+                {localStorage.getItem("hospital_name")}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {localStorage.getItem("hospital_address")}
+              </Typography>
+            </CardContent>
+          </Card>
+        </center>
+        &nbsp;
+      </div>
+      <Grid
+        container
+        spacing={4}
+        className={cards.gridContainer}
+        justifyContent="center"
+      >
+        {room_menu.map(mapCards)}
+      </Grid>
+    </div>
+  );
+}
+
+export default OutlinedCard;
