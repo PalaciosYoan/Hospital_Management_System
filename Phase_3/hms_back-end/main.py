e42a431044285d0ccef85eed29be6c74984cce1c
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2021-12-09 00:46:05 -0800

    a

--- Phase_3/hms_back-end/main.py
+++ Phase_3/hms_back-end/main.py
@@ -408,6 +408,10 @@ class getRooms(Resource):
             elif action == 'room_not_filled':
                 h_name = json.loads(request.data)['hospital_name']
                 return db_manager.get_rooms_not_filled(h_name)
+            elif action == 'room_nurse_not_fill':
+                n_id = json.loads(request.data)['n_id']
+                h_id = json.loads(request.data)['h_id']
+                db_manager.get_rooms_not_filled_by_nurse(n_id, h_id)
         except:
             action = json.loads(request.data)['formInput']['queryType']
             if action == 'delete':
